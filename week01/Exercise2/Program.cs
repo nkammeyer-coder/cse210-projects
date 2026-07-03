57e7cad9dedb2b1f398d4fc21d31bd9d9fd81b8b
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-03 08:57:16 -0700

    completed excersize 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int percentage = int.Parse(number);
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is an \"{grade}\"");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Yay! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you didn't pass. Better luck next time.");
+        }
     }
 }
