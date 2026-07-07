7523dda1197cbaab60b463879b1b3e58f6b4e145
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-07 12:23:21 -0700

    added punctuation

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -8,6 +8,6 @@ public class Job
     public int _endYear = 2026;
     public void Display()
     {
-        Console.WriteLine(${_jobTitle} ({_company}) {_startYear} - {_endYear});
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
     }
 }
