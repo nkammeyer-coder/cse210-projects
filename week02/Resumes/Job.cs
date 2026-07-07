278401dbee65a489a1cbf95bda204edbc3b0e204
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-07 12:21:18 -0700

    created Job.cs

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Job
+{
+    public string _jobTitle = "Software Developer";
+    public string _company = "Apple";
+    public int _startYear = 2015;
+    public int _endYear = 2026;
+    public void Display()
+    {
+        Console.WriteLine(${_jobTitle} ({_company}) {_startYear} - {_endYear});
+    }
+}
