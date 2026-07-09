fe2cf4eb03f79a80a92dc82b8390726a15e208c9
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-08 20:15:34 -0700

    corrected entry for _userText

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class Journal
 {
@@ -14,6 +15,35 @@ public class Journal
         foreach (Entry entry in _entries)
         {
             entry.Display();
+        }
+    }
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date} | {entry._promptText} | {entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(" | ");
+
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._userText = parts[2];
+
+            _entries.Add(entry);
         }
     }
 }
