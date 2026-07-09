3e641f3dbf7498694b49dc12af5b64301d806100
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-08 20:16:17 -0700

    updated userText

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -23,7 +23,7 @@ public class Journal
         {
             foreach (Entry entry in _entries)
             {
-                outputFile.WriteLine($"{entry._date} | {entry._promptText} | {entry._entryText}");
+                outputFile.WriteLine($"{entry._date} | {entry._promptText} | {entry._userText}");
             }
         }
     }
