ba9002b44b170e369633ca1bffa0ee641c14eeba
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-08 20:47:43 -0700

    started Program.cs with the choices list

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        string choice = "";
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please choose an option from the choices below:");
+            Console.WriteLine("1. Write a Message in Your Journal");
+            Console.WriteLine("2. Display Your Journal Records");
+            Console.WriteLine("3. Load a Record");
+            Console.WriteLine("4. Save Your Progress");
+            Console.WriteLine("5. Quit Journal Program");
+            Console.Write("What would you like to do? ");
+
+            choice = Console.ReadLine();
+        }
     }
 }
