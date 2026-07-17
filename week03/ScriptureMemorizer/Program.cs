6f7d526fa7b42bc2241bc2ca29449f85443928d4
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-16 18:45:30 -0700

    completed Program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("");
+        Reference reference = new Reference("Ether 12:27");
+        Scripture scripture = new Scripture("And if men come unto me I will show unto them their weakness. I giveunto men weakness that they may be hunble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.");
 
         string scriptureText = "";
 
