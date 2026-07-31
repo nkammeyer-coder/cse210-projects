1002a6162fab9c7b096e514a20b9dd169b36889d
nkammeyer-coder <nkammeyer@gmail.com>
2026-07-30 17:15:14 -0700

    completed Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        string choice = "";
+
+        while (choice !="4")
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Begin Breathing Activity");
+            Console.WriteLine("  2. Begin Reflection Activity");
+            Console.WriteLine("  3. Begin Listing Activity");
+            Console.WriteLine("  4. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+            }
+            else if (choice == "2")
+            {
+                ReflectionActivity reflectionActivity = new ReflectionActivity();
+                reflectionActivity.Run();
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();
+            }
+            else if (choice != "4")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a number from 1 - 4 to continue.");
+                Console.ReadLine();
+            }
+        }
     }
 }
